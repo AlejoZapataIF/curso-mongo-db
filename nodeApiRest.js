b131510ae634d3efad4d6f4f63c495b88aba2353
AlejoZapataIF <144501421+AlejoZapataIF@users.noreply.github.com>
2023-10-18 21:00:44 -0500

    Create nodeApiRest.js

---
+++ nodeApiRest.js
@@ -0,0 +1,25 @@
+const express = require('express')
+// Creamos una instancia de app
+// http y https
+const app = express()
+
+// API = recibe las peticiones  = requests externos
+// GET
+app.get('/', function (req, res) {
+  res.send('Hola Clase')
+})
+
+// POST = enviar desde el front a este servidor data
+// habilitamos la API para recibir tipos de datos JSON
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.post('/enviardata',  (req, res) => {
+  // recibimos el body enviado desde el front
+  let data = req.body;
+  // enviamos respuesta al front
+  res.send('Data recibida en el servidor: ' + JSON.stringify(data) )
+})
+
+app.listen(3000)
